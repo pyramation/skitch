c7edf8836fbe24936ae17a946b0e0edd3ff32741
Dan Lynch <pyramation@gmail.com>
2018-02-22 14:32:35 -0800

    update

--- packages/skitch/src/cmds/template.ts
+++ packages/skitch/src/cmds/template.ts
@@ -1,31 +1,33 @@
 import { exec } from 'child_process';
 import { filter } from 'fuzzy';
 import { prompt } from 'skitch-prompt';
+import templates from 'skitch-templates';
 
 // sqitch add appschema -n 'Add schema for all flipr objects.'
 
-export const template = async (argv, schemas) => {
-  const searchTemplates = (answers: object, input: string) => {
-    input = input || '';
-    return new Promise(function(resolve) {
-      setTimeout(function() {
-        var fuzzyResult = filter(input, Object.keys(schemas));
-        resolve(
-          fuzzyResult.map(function(el) {
-            return el.original;
-          })
-        );
-      }, 25);
-    });
-  };
-  const questions = [
-    {
-      type: 'autocomplete',
-      name: 'template',
-      message: 'what do you want to create?',
-      source: searchTemplates,
-    },
-  ];
+const searchTemplates = (answers: object, input: string) => {
+  input = input || '';
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      var fuzzyResult = filter(input, Object.keys(templates));
+      resolve(
+        fuzzyResult.map(function(el) {
+          return el.original;
+        })
+      );
+    }, 25);
+  });
+};
+const questions = [
+  {
+    type: 'autocomplete',
+    name: 'template',
+    message: 'what do you want to create?',
+    source: searchTemplates,
+  },
+];
+
+export const template = async argv => {
   const result = await prompt(questions, argv);
   console.log(result);
 };
