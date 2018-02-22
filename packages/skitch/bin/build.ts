7ac8e2445966888f5bc45075757c22c7c5921473
Dan Lynch <pyramation@gmail.com>
2018-02-22 14:51:17 -0800

    update

---
+++ packages/skitch/bin/build.ts
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob').sync;
+
+const srcDir = path.resolve(`${__dirname}/../src/cmds`);
+
+const paths = glob(`${srcDir}/**.ts`).map(file => {
+  const [, name] = file.match(/\/([a-zA-Z]+)\.ts/);
+  return {
+    name,
+    path: file.replace(srcDir, './cmds').replace(/\.ts$/, ''),
+  };
+});
+
+const imports = paths
+  .map(f => {
+    return [`import * as ${f.name} from '${f.path}';`];
+  })
+  .join('\n');
+
+const out = `
+    ${imports}
+    export default {
+      ${paths.map(a => a.name).join(',')}
+    }
+  `;
+
+fs.writeFileSync(`${__dirname}/../src/index.ts`, out);
---
+++ packages/skitch/src/index.ts
@@ -0,0 +1,8 @@
+import * as add from './cmds/add';
+import * as init from './cmds/init';
+import * as template from './cmds/template';
+export default {
+  add,
+  init,
+  template,
+};
