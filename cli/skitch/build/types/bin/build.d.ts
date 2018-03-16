87fb407a79897bb21c3099a39fe7a52da5c07924
Dan Lynch <pyramation@gmail.com>
2018-03-15 18:10:49 -0700

    update

--- cli/skitch/build/types/bin/build.d.ts
+++
@@ -1,7 +0,0 @@
-declare const fs: any;
-declare const path: any;
-declare const glob: any;
-declare const srcDir: any;
-declare const paths: any;
-declare const imports: any;
-declare const out: string;
--- cli/skitch/build/types/src/cli.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const searchCmds: (answers: object, input: string) => Promise<{}>;
-export declare const skitch: (argv: any) => Promise<void>;
