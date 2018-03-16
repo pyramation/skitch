7cc64d8443c92ec3eea514740230e8250bd2fce8
Dan Lynch <pyramation@gmail.com>
2018-03-15 18:12:26 -0700

    update

--- cli/skitch/src/index.ts
+++ cli/skitch/src/index.ts
@@ -8,13 +8,13 @@ import generate from './cmds/generate';
 import info from './cmds/info';
 import init from './cmds/init';
 import install from './cmds/install';
+import maketest from './cmds/maketest';
 import plan from './cmds/plan';
 import rename from './cmds/rename';
 import revert from './cmds/revert';
 import start from './cmds/start';
-import test from './cmds/test';
 import verify from './cmds/verify';
 export default {
-  add,bundle,createdb,deploy,dropdb,generate,info,init,install,plan,rename,revert,start,test,verify
+  add,bundle,createdb,deploy,dropdb,generate,info,init,install,maketest,plan,rename,revert,start,verify
 }
   
