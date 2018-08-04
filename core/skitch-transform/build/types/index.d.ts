1275c4f87345adc8eda1b268eb4e7ec72664500a
Dan Lynch <pyramation@gmail.com>
2018-08-04 01:35:29 -0700

    updated

--- core/skitch-transform/build/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ObjectHash {
-    [type: string]: string;
-}
-export interface ReplaceHash {
-    [type: string]: Function | ObjectHash;
-}
-export declare const transform: (statement: string, props: ReplaceHash) => any;
