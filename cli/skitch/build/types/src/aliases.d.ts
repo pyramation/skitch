164acbd896e2fbad063a2a1c28fdfc052c69eede
Dan Lynch <pyramation@gmail.com>
2018-08-16 17:18:22 -0700

    update

---
+++ cli/skitch/build/types/src/aliases.d.ts
@@ -0,0 +1,21 @@
+declare const _default: {
+    add: any;
+    bundle: any;
+    createdb: any;
+    deploy: any;
+    dropdb: any;
+    format: any;
+    generate: string[];
+    info: any;
+    init: any;
+    install: any;
+    maketest: any;
+    package: any;
+    plan: any;
+    rename: any;
+    resolve: any;
+    revert: any;
+    start: any;
+    verify: any;
+};
+export default _default;
