27debb332ae57bcc17e97d09ded0761b2595f916
Dan Lynch <pyramation@gmail.com>
2018-08-21 17:04:05 -0600

    update

--- core/skitch-testing/build/src/wrapper.js
+++ core/skitch-testing/build/src/wrapper.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function PgpWrapper(db, ctx) {
+    var _this = this;
     this.db = db;
     this._ctx = Object.keys(ctx).reduce(function (m, el) {
         m.push("SELECT set_config('" + el + "', '" + ctx[el] + "', true);");
         return m;
     }, []).join('\n');
-    ;
+    ['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(function (op) {
+        _this[op] = function (ctx, query, values) {
+            return db[op](this._ctx + query, values);
+        };
+    });
 }
 exports.default = PgpWrapper;
-['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(function (op) {
-    PgpWrapper.prototype[op] = function (ctx, query, values) {
-        return this.db[op](this._ctx + query, values);
-    };
-});
 //# sourceMappingURL=wrapper.js.map
--- core/skitch-testing/src/wrapper.ts
+++ core/skitch-testing/src/wrapper.ts
@@ -4,11 +4,9 @@ export default function PgpWrapper(db, ctx) {
     m.push(`SELECT set_config('${el}', '${ctx[el]}', true);`);
     return m;
   }, []).join('\n');
-  ;
+  ['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(op=>{
+    this[op] = function(ctx, query, values) {
+      return db[op](this._ctx + query, values);
+    };
+  });
 }
-
-['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(op=>{
-  PgpWrapper.prototype[op] = function(ctx, query, values) {
-    return this.db[op](this._ctx + query, values);
-  };
-});
