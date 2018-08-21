85d06daa73cfbf981ac709e4731cda862a038f1a
Dan Lynch <pyramation@gmail.com>
2018-08-21 17:10:28 -0600

    update

--- core/skitch-testing/build/src/wrapper.js
+++ core/skitch-testing/build/src/wrapper.js
@@ -1,17 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function PgpWrapper(db, ctx) {
-    var _this = this;
     this.db = db;
-    this._ctx = Object.keys(ctx).reduce(function (m, el) {
-        m.push("SELECT set_config('" + el + "', '" + ctx[el] + "', true);");
-        return m;
-    }, []).join('\n');
-    ['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(function (op) {
-        _this[op] = function (ctx, query, values) {
-            return db[op](this._ctx + query, values);
-        };
-    });
+    this._ctxStmts = function (ctx) {
+        return Object.keys(ctx).reduce(function (m, el) {
+            m.push("SELECT set_config('" + el + "', '" + ctx[el] + "', true);");
+            return m;
+        }, []).join('\n');
+    };
 }
 exports.default = PgpWrapper;
+['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(function (op) {
+    PgpWrapper.prototype[op] = function (query, values) {
+        return this.db[op](this._ctxStmts + query, values);
+    };
+});
 //# sourceMappingURL=wrapper.js.map
--- core/skitch-testing/src/wrapper.ts
+++ core/skitch-testing/src/wrapper.ts
@@ -1,12 +1,16 @@
 export default function PgpWrapper(db, ctx) {
   this.db = db;
-  this._ctx = Object.keys(ctx).reduce((m,el)=>{
-    m.push(`SELECT set_config('${el}', '${ctx[el]}', true);`);
-    return m;
-  }, []).join('\n');
-  ['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(op=>{
-    this[op] = function(ctx, query, values) {
-      return db[op](this._ctx + query, values);
-    };
-  });
+
+  this._ctxStmts = function(ctx) {
+    return Object.keys(ctx).reduce((m,el)=>{
+      m.push(`SELECT set_config('${el}', '${ctx[el]}', true);`);
+      return m;
+    }, []).join('\n');
+  };
 }
+
+['none', 'one', 'many', 'oneOrNone', 'manyOrNone', 'any', 'result'].forEach(op=>{
+  PgpWrapper.prototype[op] = function(query, values) {
+    return this.db[op](this._ctxStmts + query, values);
+  };
+});
