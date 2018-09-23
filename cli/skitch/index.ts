4f82644dc1c96e8fa95cd052df014fcd6a202e5d
Dan Lynch <pyramation@gmail.com>
2018-09-22 23:50:47 -0700

    version

--- cli/skitch/index.ts
+++ cli/skitch/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { skitch } from './src/cli';
 var argv = require('minimist')(process.argv.slice(2));
+var readFileSync = require('fs').readFileSync;
 
 (async () => {
   if (argv.v) {
-    return console.log( require(__dirname + '/package.json').version );
+    return console.log( JSON.parse(readFileSync(__dirname + '/package.json').toJSON()).version );
   }
   await skitch(argv);
 })();
